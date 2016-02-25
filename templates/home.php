5f213ce9d5e5737d0cc8b2649ef50c8d4d35fb78
Markus Andresen <markua@stud.ntnu.no>
2016-02-25 23:35:42 +0100

    major update v2

--- templates/home.php
+++ templates/home.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 if(empty($_SESSION['logged_in'])) {
     header('Location: ../index.php');
     exit;
@@ -11,86 +10,34 @@ if(empty($_SESSION['logged_in'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../css/mycss.css">
-    <title>Home</title>
-    <script>
-
-        $(document).ready(function () {
-            $('#sidebar-btn').click(function(){
-                $('#sidebar').toggleClass('visible');
-            })
-
-            $('#sidebar-btn').click(function(){
-                $('#container').toggleClass('active-bar');
-            })
-        });
-
-        function logout(){
-            window.location.href = 'logout.php'
-        }
-        window.onload = function(){
-            var name = "<?php echo $_SESSION["name"]; ?>";
-            document.getElementById("name").innerHTML = "Welcome " + name;
-        }
-
-
-        $(document).ready(function(){
-            $("#records").click(function(){
-                $("#container").load('registerForm.php');
-            });
-        });
-
-
-    </script>
+    <?php include("head.php")?>
 </head>
 <body>
-    <nav>
-        <div id="sidebar-btn">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <a href="#" class="glyphicon glyphicon-cog white" aria-hidden="true" style="float: right; margin-right: 40px; margin-top: 15px; font-size: 1.1em"></a>
-        <a href="#" class="glyphicon glyphicon-envelope white" aria-hidden="true" style="float: right; margin-right: 35px; margin-top: 15px; font-size: 1.1em"></a>
-        <a href="#" class="glyphicon glyphicon-user white" aria-hidden="true" style="float: right; margin-right: 35px; margin-top: 15px; font-size: 1.1em"></a>
 
-    </nav>
+<nav>
+    <?php include("nav.php")?>
+</nav>
 
-    <div id="sidebar" class="visible">
+<div id="sidebar" class="visible">
+    <?php include("sidebar.php")?>
+</div>
+
+<div id="container">
+    <div class="inner">
+    <br>
+    <div class="alert alert-success" style="width: 50%; text-align: center; margin: 0 auto;" role="alert">
         <br>
-        <ul>
-            <li id ="home"><span class="glyphicon glyphicon-home white" style="margin-right: 10px; font-size: 0.9em"></span>Home</li>
-            <hr>
-            <li><span class="glyphicon glyphicon-plus-sign white" style="margin-right: 10px; font-size: 0.9em"></span>New Session</li>
-            <li id ="records"><span class="glyphicon glyphicon-list-alt white" style="margin-right: 10px; font-size: 0.9em"></span>Records</li>
-            <li><span class="glyphicon glyphicon-stats white" style="margin-right: 10px; font-size: 0.9em"></span>Progress</li>
-            <li><span class="glyphicon glyphicon-calendar white" style="margin-right: 10px; font-size: 0.9em"></span>Calendar</li>
-
-        </ul>
-    </div>
-
-    <div id="container">
+        <span id="name">Welcome <?php echo $_SESSION["name"]; ?></span>
         <br>
-        <div class="alert alert-success" style="width: 50%;  margin: 0 auto;" role="alert">
-            <br>
-            <span id="name"></span>
-            <br>
-            <br>
-            <br>
-            <br>
-            <button class="btn btn-warning" onclick="logout()"> Logout </button>
-            <br>
-        </div>
- <br>
-
-
+        <br>
+        <br>
+        <br>
+        <button class="btn btn-warning" onclick="logout()"> Logout </button>
+        <br>
     </div>
+    <br>
+    </div>
+</div>
 
 </body>
 </html>
--- templates/records.php
+++ templates/records.php
@@ -1,2 +1,207 @@
 <?php
 session_start();
+include_once("connection.php");
+if(empty($_SESSION['logged_in'])) {
+    header('Location: ../index.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("head.php")?>
+</head>
+<body>
+
+<nav>
+    <?php include("nav.php")?>
+</nav>
+
+<div id="sidebar" class="visible">
+    <?php include("sidebar.php")?>
+</div>
+
+
+<div class="container active-bar">
+    <div class="inner">
+    <?php
+    session_start();
+    include_once("connection.php");
+    $id = $_SESSION["personid"];
+    $query1 = mysql_query(
+    /** @lang MYSQL */
+    "select exercise_name,kg,reps
+    from records
+    join persons on personid=personid_fk
+    join exercise on exerciseid=exerciseid_fk
+    where personid ='$id'
+    group by kg,exercise_name
+    having (exercise_name,kg) in
+    (select exercise_name,max(kg) from records
+    join exercise on exerciseid = exerciseid_fk
+    join persons on personid = personid_fk
+    where personid = '$id'
+    group by exerciseid_fk)
+    order by kg desc");
+
+    $query2 = mysql_query(
+    /** @lang MYSQL */
+    "select kg,reps,date
+    from records
+    join persons on personid=personid_fk
+    join exercise on exerciseid=exerciseid_fk
+    left join training_session on sessionid=sessionid_fk
+    where personid='$id' and exerciseid=1
+    order by kg desc");
+
+    $query3 = mysql_query(
+    /** @lang MYSQL */
+    "select kg,reps,date
+    from records
+    join persons on personid=personid_fk
+    join exercise on exerciseid=exerciseid_fk
+    left join training_session on sessionid=sessionid_fk
+    where personid='$id' and exerciseid=2
+    order by kg desc");
+
+    $query4 = mysql_query(
+    /** @lang MYSQL */
+    "select kg,reps,date
+    from records
+    join persons on personid=personid_fk
+    join exercise on exerciseid=exerciseid_fk
+    left join training_session on sessionid=sessionid_fk
+    where personid='$id' and exerciseid=3
+    order by kg desc");
+    ?>
+
+    <div class="panel">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><h4>Main Lifts - PR <span class="glyphicon glyphicon-star-empty" style="float: right"></span</h4></div>
+        <!-- Table -->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Exercise Name</th>
+                <th>Weight (kg)</th>
+                <th>Repetitions</th>
+            </tr>
+            </thead>
+            <?php
+            while ($row = mysql_fetch_array($query1)) {
+                echo "<tr>";
+                echo "<td>".$row[exercise_name]."</td>";
+                echo "<td>".$row[kg]."</td>";
+                echo "<td>".$row[reps]."</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+
+
+    </div>
+
+    <br>
+    <div class="panel">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><h4>Squat - PR</h4></div>
+
+        <!-- Table -->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Weight (kg)</th>
+                <th>Repetitions</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <?php
+            while ($row = mysql_fetch_array($query2)) {
+                echo "<tr>";
+                echo "<td>".$row[kg]."</td>";
+                echo "<td>".$row[reps]."</td>";
+                if($row[date] != null){
+                    echo "<td>".$row[date]."</td>";
+                }
+                else{
+                    echo "<td>Not Registered</td>";
+                }
+
+                echo "</tr>";
+            }
+
+            ?>
+        </table>
+    </div>
+
+    <br>
+    <div class="panel">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><h4>Deadlift - PR</h4></div>
+
+        <!-- Table -->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Weight (kg)</th>
+                <th>Repetitions</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <?php
+            while ($row = mysql_fetch_array($query3)) {
+                echo "<tr>";
+                echo "<td>".$row[kg]."</td>";
+                echo "<td>".$row[reps]."</td>";
+                if($row[date] != null){
+                    echo "<td>".$row[date]."</td>";
+                }
+                else{
+                    echo "<td>Not Registered</td>";
+                }
+
+                echo "</tr>";
+            }
+
+            ?>
+        </table>
+    </div>
+
+
+    <br>
+    <div class="panel">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><h4>Bench Press - PR</h4></div>
+
+        <!-- Table -->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Weight (kg)</th>
+                <th>Repetitions</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <?php
+            while ($row = mysql_fetch_array($query4)) {
+                echo "<tr>";
+                echo "<td>".$row[kg]."</td>";
+                echo "<td>".$row[reps]."</td>";
+                if($row[date] != null){
+                    echo "<td>".$row[date]."</td>";
+                }
+                else{
+                    echo "<td>Not Registered</td>";
+                }
+
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </div>
+    </div>
+</div>
+
+</body>
+</html>
