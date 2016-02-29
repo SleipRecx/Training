de7ebbc852ddd84b5204fa6945f004838bd596d3
Markus Andresen <markua@stud.ntnu.no>
2016-03-01 00:13:27 +0100

    protection against DB delete implemented

--- templates/delexercise.php
+++ templates/delexercise.php
@@ -2,16 +2,34 @@
 session_start();
 include_once("connection.php");
 $deleteID = $_POST["exerciseid"];
-$sql = /** @lang MYSQL */
-    "DELETE FROM exercise
-    WHERE exerciseid = '$deleteID'";
+
+$sql= /** @lang MYSQL */
+    "select personid_fk,exerciseid
+    from exercise
+    where exerciseid = '$deleteID'";
 
 $retval = mysql_query($sql);
-echo $retval;
-if(! $retval) {
-    die('Could not enter data: ' . mysql_error());
+$values = mysql_fetch_array($retval);
+$personid_fk = $values["personid_fk"];
+
+if($personid_fk != $_SESSION["personid"]){
+    echo "<h2>You trying to hack my shit??</h2>";
+    header("refresh:5;url=http://www.theworldsworstwebsiteever.com/" );
+
 }
 else{
-    echo "Entered data successfully\n";
-    header("Location: exercise.php");
+    $sql = /** @lang MYSQL */
+        "DELETE FROM exercise
+    WHERE exerciseid = '$deleteID'";
+
+    $retval = mysql_query($sql);
+    echo $retval;
+    if(! $retval) {
+        die('Could not enter data: ' . mysql_error());
+    }
+    else{
+        echo "Entered data successfully\n";
+        header("Location: exercise.php");
+    }
+
 }
