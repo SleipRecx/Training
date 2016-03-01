3ab6d6aa6552b7346e3220f2e209278e1a4f7638
Markus Andresen <markua@stud.ntnu.no>
2016-03-01 20:42:32 +0100

    link to session.php added

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -2,7 +2,8 @@
 <ul>
     <a href="home.php"><li id ="home"><span class="glyphicon glyphicon-home white" style="margin-right: 10px; font-size: 0.9em"></span>Home</li></a>
     <hr>
-    <li><span class="glyphicon glyphicon-plus-sign white" style="margin-right: 10px; font-size: 0.9em"></span>New Session</li>
+
+    <a href="session.php"><li><span class="glyphicon glyphicon-plus-sign white" style="margin-right: 10px; font-size: 0.9em"></span>Sessions</li></a>
     <a href="records.php"><li><span class="glyphicon glyphicon-list-alt white" style="margin-right: 10px; font-size: 0.9em"></span>Records</li></a>
     <li><span class="glyphicon glyphicon-stats white" style="margin-right: 10px; font-size: 0.9em"></span>Progress</li>
     <li><span class="glyphicon glyphicon-calendar white" style="margin-right: 10px; font-size: 0.9em"></span>Calendar</li>
